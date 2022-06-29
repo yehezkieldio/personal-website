624eba42481435699a7b8e33dbd95c04a51752f0
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-29 09:37:59 +0800

    feat: more things

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import { theme } from "../utils/theme";
+
 function PortfolioApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
-                <ChakraProvider resetCSS>
+                <ChakraProvider resetCSS theme={theme}>
                     <Component {...pageProps} />
                 </ChakraProvider>
             </AnimatePresence>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
+import { Container, Heading, Text } from "@chakra-ui/react";
 
-import { Container, Heading } from "@chakra-ui/react";
+import Navigation from "../components/Navigation";
 
 const Home: NextPage = () => {
     return (
         <>
-            <Container>
+            <Navigation />
+            <Container maxWidth="1230px" marginY="24px">
                 <Heading>Hello, world.</Heading>
+                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Text>
             </Container>
         </>
     );
