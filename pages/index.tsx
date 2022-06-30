de0fc6932d914eea119c129247fa813a0781bf5c
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-30 13:51:23 +0800

    feat: basic introduction in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPage } from "next";
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, Box } from "@chakra-ui/react";
 
 import PageContainer from "../components/PageContainer";
 
 const Index: NextPage = () => {
     return (
         <PageContainer title="yehezkieldio">
-            <Heading>Index</Heading>
-            <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit sequi tempore magnam.</Text>
+            <Box maxWidth="820px" paddingTop="64px" paddingBottom="64px">
+                <Text fontSize={{ base: "sm", md: "md" }} fontWeight="medium">
+                    Hello, I'm Yehezkiel Dio.
+                </Text>
+                <Heading fontSize={{ base: "3xl", md: "5xl" }} fontWeight="bold" lineHeight="shorter">
+                    I am a web developer based in Balikpapan, Indonesia.
+                </Heading>
+                <Text fontSize={{ base: "xs", md: "md" }} lineHeight="tall" marginTop="8px">
+                    Currently working on various projects online, while attending a vocational high school as a software
+                    engineering major student.
+                </Text>
+            </Box>
         </PageContainer>
     );
 };
-
 export default Index;
