332822f7dc73d44552f061df69fbd3f877427bce
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-30 09:45:49 +0800

    chore: use swc

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    crossOrigin: "anonymous",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
