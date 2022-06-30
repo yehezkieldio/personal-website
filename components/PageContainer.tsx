835e69b2f6dc125d34ef84afe910814084687cbd
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-30 13:50:34 +0800

    feat: footer component

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -4,6 +4,7 @@ import { Container, chakra } from "@chakra-ui/react";
 
 import Navigation from "./Navigation";
 import PageTransition from "./PageTransition";
+import Footer from "./Footer";
 
 const ChakraMain = chakra(chakra.div);
 
@@ -14,9 +15,10 @@ export default function PageContainer({ title, children }: { title: string; chil
                 <title>{title}</title>
             </Head>
             <Navigation />
-            <Container maxWidth="1230px" marginY="24px">
+            <Container maxWidth="1130px" marginY="24px">
                 <PageTransition>{children}</PageTransition>
             </Container>
+            <Footer />
         </ChakraMain>
     );
 }
