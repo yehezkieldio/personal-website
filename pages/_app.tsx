9868dd618e69dcfb7543f77fc752214911a9434e
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-30 13:49:26 +0800

    chore: axios and react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,30 @@
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../utils/theme";
 
 function PortfolioApp({ Component, pageProps }: AppProps) {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false,
+                refetchOnMount: false,
+                refetchOnReconnect: false,
+                retry: false,
+                staleTime: 1000 * 60 * 60 * 24,
+            },
+        },
+    });
+
     return (
         <>
             <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
                 <ChakraProvider resetCSS theme={theme}>
-                    <Component {...pageProps} />
+                    <QueryClientProvider client={queryClient}>
+                        <Component {...pageProps} />
+                    </QueryClientProvider>
                 </ChakraProvider>
             </AnimatePresence>
         </>
